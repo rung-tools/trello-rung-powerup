c83f51ed4a7188bff650d21d2956bf481cf33245
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2017-06-21 16:18:04 -0300

    Public files on /

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors({ origin: 'https://trello.com' }));
 
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 
 app.listen(port, () => {
     console.log(`Power-Up running on port ${port}...`);
