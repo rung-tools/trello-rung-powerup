1224a808680cf72386eb1c4410cc4496d703cd45
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-22 17:18:07 -0300

    Carcando ficha

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const publicPath = path.join(__dirname, 'public');
+const resourcesPath = path.join(publicPath, 'resources')
 
 const killPlugin = function () {
     this.plugin('done', stats => {
@@ -15,7 +16,10 @@ const killPlugin = function () {
 const staticAssetsPlugin = new CopyWebpackPlugin([
     { from: path.join(__dirname, 'assets/html/auth.html'), to: publicPath },
     { from: path.join(__dirname, 'assets/html/index.html'), to: publicPath },
-    { from: path.join(__dirname, 'manifest.json'), to: publicPath }
+    { from: path.join(__dirname, 'manifest.json'), to: publicPath },
+    { from: path.join(__dirname, 'assets/images/rung-full-white.png'), to: resourcesPath },
+    { from: path.join(__dirname, 'assets/images/rung-white.png'), to: resourcesPath },
+    { from: path.join(__dirname, 'assets/images/rung.png'), to: resourcesPath }
 ]);
 
 module.exports = {
