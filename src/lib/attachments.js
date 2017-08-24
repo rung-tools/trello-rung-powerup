14e4e448144b6fc2cfe3ee7a98100f8132f56356
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-24 16:00:29 -0300

    Auth message on error

--- src/lib/attachments.js
+++ src/lib/attachments.js
@@ -1,14 +1,21 @@
 import { getAlerts } from './rung';
 
-/* global TrelloPowerUp */
+/* global TrelloPowerUp, document */
 const trello = TrelloPowerUp.iframe();
 const sessionToken = trello.arg('sessionToken');
 
 trello.render(() => {
     const tempId = '593bdb87b43ac0601189b497';
+    const content = document.getElementById('content');
     getAlerts(tempId, sessionToken)
         .then(alerts => {
-            document.getElementById('content').innerHTML = alerts.map(alert => alert.content).join('<br />');
+            content.innerHTML = alerts.map(alert => alert.content).join('<br />');
+        })
+        .catch(err => {
+            if (err.status === 401 || err.status === 403) {
+                const message = 'Authorize your Rung account on Power-Up panel';
+                content.innerHTML = message;
+            }
         })
         .then(() => {
             trello.sizeTo('#content');
