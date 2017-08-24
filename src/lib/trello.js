43ae271c5e6a12e14bed85932801d39151b6e985
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-24 09:25:49 -0300

    Remove conditional

--- src/lib/trello.js
+++ src/lib/trello.js
@@ -36,23 +36,11 @@ const listExtensions = trello => getExtensions()
 /* global TrelloPowerUp */
 TrelloPowerUp.initialize({
     'attachment-sections': function(t, options){
-        // options.entries is a list of the attachments for this card
-        // you can look through them and 'claim' any that you want to
-        // include in your section.
-
-        // we will just claim urls for Yellowstone
         var claimed = options.entries.filter(function (attachment) {
           return attachment.url.indexOf('http://www.nps.gov/yell/') === 0;
         });
 
-        // you can have more than one attachment section on a card
-        // you can group items together into one section, have a section
-        // per attachment, or anything in between.
-        if (claimed && claimed.length > 0) {
-          // if the title for your section requires a network call or other
-          // potentially lengthy operation you can provide a function for the title
-          // that returns the section title. If you do so, provide a unique id for
-          // your section
+        // if (claimed && claimed.length > 0) {
           return [{
             id: 'Yellowstone', // optional if you aren't using a function for the title
             claimed: claimed,
@@ -66,9 +54,6 @@ TrelloPowerUp.initialize({
               height: 230
             }
           }];
-        } else {
-          return [];
-        }
       },
     'board-buttons': () => [{
         icon: './resources/rung-white.png',
