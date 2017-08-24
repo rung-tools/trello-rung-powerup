f1bf66019b2ca49e9620dd6aced69df0ee9cae29
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-24 10:54:49 -0300

    Pluck content

--- src/lib/attachments.js
+++ src/lib/attachments.js
@@ -8,6 +8,6 @@ trello.render(() => {
     const tempId = '593bdb87b43ac0601189b497';
     getAlerts(tempId, sessionToken)
         .then(alerts => {
-            document.getElementById('content').innerHTML = alerts.map(alerts.content).join('<br />');
+            document.getElementById('content').innerHTML = alerts.map(alert => alert.content).join('<br />');
         });
 });
