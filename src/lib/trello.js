6b0926ae1f13c610b5b392e657acec329484bf71
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-25 11:55:01 -0300

    Dynamic card id

--- src/lib/trello.js
+++ src/lib/trello.js
@@ -6,10 +6,7 @@ import { rung, getExtensions } from './rung';
 const GRAY_ICON = './resources/rung-gray.png';
 const agent = promisifyAgent(superagent, Promise);
 
-const extensionModal = (name, title) => (trello, options) => {
-
-    console.log(options);
-
+const extensionModal = (name, title, card) => trello =>
     trello.get('board', 'private', 'sessionToken')
         .then(sessionToken =>
             trello.modal({
@@ -17,15 +14,14 @@ const extensionModal = (name, title) => (trello, options) => {
                 accentColor: '#0067B0',
                 fullscreen: false,
                 height: 540,
-                args: { sessionToken },
+                args: { sessionToken, card },
                 title
             }));
-        }
 
-const listExtensions = trello => getExtensions()
+const listExtensions = card => trello => getExtensions()
     .then(extensions => {
         const items = extensions.map(({ name, title }) => ({
-            text: title, callback: extensionModal(name, title) }));
+            text: title, callback: extensionModal(name, title, card) }));
         return trello.popup({
             title: 'Extensions',
             items,
@@ -99,10 +95,10 @@ TrelloPowerUp.initialize({
         text: 'Rung',
         url: 'https://app.rung.com.br/'
     }],
-    'card-buttons': () => [{
+    'card-buttons': (trello, options) => [{
         icon: GRAY_ICON,
         text: 'Link to Rung',
-        callback: listExtensions
+        callback: listExtensions(options.context.card)
     }],
     'authorization-status': trello => trello.get('board', 'private', 'sessionToken')
         .then(sessionToken => sessionToken
