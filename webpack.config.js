51b60e3f6ea3e26c84e86a1258e9c3b1e6f893bd
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-25 16:42:15 -0300

    To build faster

--- webpack.config.js
+++ webpack.config.js
@@ -52,21 +52,21 @@ module.exports = {
                         options: {
                             name: 'resources/[name].[ext]'
                         }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            query: {
-                                bypassOnDebug: true,
-                                optipng: {
-                                    optimizationLevel: 7
-                                },
-                                gifsicle: {
-                                    interlaced: true
-                                }
-                            }
-                        }
                     }
+                    // {
+                    //     loader: 'image-webpack-loader',
+                    //     options: {
+                    //         query: {
+                    //             bypassOnDebug: true,
+                    //             optipng: {
+                    //                 optimizationLevel: 7
+                    //             },
+                    //             gifsicle: {
+                    //                 interlaced: true
+                    //             }
+                    //         }
+                    //     }
+                    // }
                 ]
             }
         ]
